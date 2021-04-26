698d07a0c7881b9fc81ac451cd06db955b9caadf
Seba pc <seba_sanger@hotmail.com>
2021-04-26 11:56:16 -0300

    try redeploy to heroku

--- src/main/java/com/sanger/sprintel/error/exceptions/NewUserWithDifferentPasswordsException.java
+++ src/main/java/com/sanger/sprintel/error/exceptions/NewUserWithDifferentPasswordsException.java
@@ -8,7 +8,7 @@ public class NewUserWithDifferentPasswordsException extends RuntimeException {
 	private static final long serialVersionUID = -7978601526802035152L;
 
 	public NewUserWithDifferentPasswordsException() {
-		super("Las contraseñas no coinciden");
+		super("Password not missmatch");
 	}
 
 }
