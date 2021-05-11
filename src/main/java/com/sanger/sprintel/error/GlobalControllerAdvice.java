aae374c5a0a74d62d90ccc99bccc59469b665d8a
Seba pc <seba_sanger@hotmail.com>
2021-05-11 08:35:13 -0300

    error messages on try delete element with realted tables or try create or update duplicates

--- src/main/java/com/sanger/sprintel/error/GlobalControllerAdvice.java
+++ src/main/java/com/sanger/sprintel/error/GlobalControllerAdvice.java
@@ -3,6 +3,8 @@ package com.sanger.sprintel.error;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.sanger.sprintel.error.exceptions.EntityNotFoundException;
 import com.sanger.sprintel.error.exceptions.FindEntityByIdNotFoundException;
@@ -11,6 +13,7 @@ import com.sanger.sprintel.error.exceptions.PasswordNotMismatch;
 import com.sanger.sprintel.error.exceptions.SearchEntityNoResultException;
 import com.sanger.sprintel.error.exceptions.UserNotFoundException;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +38,32 @@ public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ApiError> handleBadRequest(Exception ex) {
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+	}
+
+	@ExceptionHandler({ ConstraintViolationException.class })
+	public ResponseEntity<ApiError> handleContrainValidationError(ConstraintViolationException ex) {
+
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Error");
+
+		String error = ex.getCause().getMessage();
+
+		// Case duplicate element
+		Pattern pattern = Pattern.compile("^Duplicate\\s\\w+\\s\\'\\w+\\'", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(error);
+		boolean matchFound = matcher.find();
+		if (matchFound) {
+			String messageDulicate = matcher.group(0);
+			String messageDulicateTrim = messageDulicate.replace("'", "");
+			apiError.setMessage(messageDulicateTrim);
+		}
+
+		// Case delete element have relations
+		if (error.startsWith("Cannot delete")) {
+			apiError.setMessage("can't delete this item, it has related items");
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+
 	}
 
 	@Override
