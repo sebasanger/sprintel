75fab5b085627cce3eebf9734d7aabb3826ca4a1
Seba pc <seba_sanger@hotmail.com>
2021-05-13 13:02:07 -0300

    method to filter and page the customers added

--- src/main/java/com/sanger/sprintel/repository/CustomerRepository.java
+++ src/main/java/com/sanger/sprintel/repository/CustomerRepository.java
@@ -2,8 +2,11 @@ package com.sanger.sprintel.repository;
 
 import com.sanger.sprintel.model.Customer;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Page<Customer> findByNameIgnoreCaseContainingOrSurnameIgnoreCaseContainingOrEmailIgnoreCaseContainingOrDniIgnoreCaseContaining(
+            String name, String surname, String email, String dni, Pageable pageable);
 }
--- src/main/java/com/sanger/sprintel/services/CustomerService.java
+++ src/main/java/com/sanger/sprintel/services/CustomerService.java
@@ -4,9 +4,15 @@ import com.sanger.sprintel.model.Customer;
 import com.sanger.sprintel.repository.CustomerRepository;
 import com.sanger.sprintel.services.base.BaseService;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService extends BaseService<Customer, Long, CustomerRepository> {
-
+    public Page<Customer> filterAndPaginateCustomer(String filter, Pageable pageable) {
+        return this.repository
+                .findByNameIgnoreCaseContainingOrSurnameIgnoreCaseContainingOrEmailIgnoreCaseContainingOrDniIgnoreCaseContaining(
+                        filter, filter, filter, filter, pageable);
+    }
 }
